813650b1cccc5689bb22c8eef7a7a37599acc0a2
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-05 15:12:22 -0400

    Add test to capitalize function, if str is a number, throw error

--- Exercise 4/capitalize.js
+++ Exercise 4/capitalize.js
@@ -1,7 +1,10 @@
 function capitalize(str) {
-  const firstLetter = str.split('').splice(0, 1).join('').toUpperCase();
-  const newStr = str.replace(str[0], firstLetter);
-  return newStr;
+  if (typeof str === 'string') {
+    const firstLetter = str.split('').splice(0, 1).join('').toUpperCase();
+    const newStr = str.replace(str[0], firstLetter);
+    return newStr;
+  }
+  throw new Error('You must write a word for the function to work');
 }
 
 module.exports = capitalize;
--- Exercise 4/capitalize.test.js
+++ Exercise 4/capitalize.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('expect');
 const capitalize = require('./capitalize.js');
 
 test('Capitalize test', () => {
@@ -6,4 +7,10 @@ test('Capitalize test', () => {
 
 test('Capitalize test 2', () => {
   expect(capitalize('adios')).toBe('Adios');
+});
+
+test('Capitalize: not a string', () => {
+  expect(() => {
+    capitalize(5);
+  }).toThrow();
 });
